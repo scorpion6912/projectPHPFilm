cefe1ef39e710c2a1ad05d258ea9429baa55e067
Rémy DAVID <remy.david@etu.univ-lyon1.fr>
2022-01-27 14:54:42 +0100

    Movie controller and BD added

---
+++ src/Entity/Movie.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MovieRepository::class)]
+class Movie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'text')]
+    private $description;
+
+    #[ORM\Column(type: 'integer')]
+    private $score;
+
+    #[ORM\Column(type: 'integer')]
+    private $votersNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getVotersNumber(): ?int
+    {
+        return $this->votersNumber;
+    }
+
+    public function setVotersNumber(int $votersNumber): self
+    {
+        $this->votersNumber = $votersNumber;
+
+        return $this;
+    }
+}
